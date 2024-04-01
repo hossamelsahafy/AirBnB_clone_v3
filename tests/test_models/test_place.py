d2646a62710944a8551477c58a8d01fe30c87802
Mohamed kamal <kmido9729@gmail.com>
2024-04-02 00:29:17 +0200

    hamo bydahy

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,200 +1,86 @@
 #!/usr/bin/python3
 """
-Contains the TestPlaceDocs classes
+Create a new view for Place objects that handles all
+default RESTFul API actions.
 """
-
-from datetime import datetime
-import inspect
-import models
-from models import place
-from models.base_model import BaseModel
-import pep8
-import unittest
-Place = place.Place
+from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
+from models import storage
+from models.city import City
+from models.place import Place
+from models.user import User
 
 
-class TestPlaceDocs(unittest.TestCase):
-    """Tests to check the documentation and style of Place class"""
-    @classmethod
-    def setUpClass(cls):
-        """Set up for the doc tests"""
-        cls.place_f = inspect.getmembers(Place, inspect.isfunction)
-
-    def test_pep8_conformance_place(self):
-        """Test that models/place.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['models/place.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_conformance_test_place(self):
-        """Test that tests/test_models/test_place.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_models/test_place.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_place_module_docstring(self):
-        """Test for the place.py module docstring"""
-        self.assertIsNot(place.__doc__, None,
-                         "place.py needs a docstring")
-        self.assertTrue(len(place.__doc__) >= 1,
-                        "place.py needs a docstring")
-
-    def test_place_class_docstring(self):
-        """Test for the Place class docstring"""
-        self.assertIsNot(Place.__doc__, None,
-                         "Place class needs a docstring")
-        self.assertTrue(len(Place.__doc__) >= 1,
-                        "Place class needs a docstring")
-
-    def test_place_func_docstrings(self):
-        """Test for the presence of docstrings in Place methods"""
-        for func in self.place_f:
-            self.assertIsNot(func[1].__doc__, None,
-                             "{:s} method needs a docstring".format(func[0]))
-            self.assertTrue(len(func[1].__doc__) >= 1,
-                            "{:s} method needs a docstring".format(func[0]))
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+                 strict_slashes=False)
+def get_places(city_id):
+    """get cities by id"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    all_places = []
+    for place in city.places:
+        all_places.append(place.to_dict())
+    return jsonify(all_places)
 
 
-class TestPlace(unittest.TestCase):
-    """Test the Place class"""
-    def test_is_subclass(self):
-        """Test that Place is a subclass of BaseModel"""
-        place = Place()
-        self.assertIsInstance(place, BaseModel)
-        self.assertTrue(hasattr(place, "id"))
-        self.assertTrue(hasattr(place, "created_at"))
-        self.assertTrue(hasattr(place, "updated_at"))
+@app_views.route('/places/<place_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_place(place_id):
+    """Get place by id."""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    return jsonify(place.to_dict())
 
-    def test_city_id_attr(self):
-        """Test Place has attr city_id, and it's an empty string"""
-        place = Place()
-        self.assertTrue(hasattr(place, "city_id"))
-        if models.storage_t == 'db':
-            self.assertEqual(place.city_id, None)
-        else:
-            self.assertEqual(place.city_id, "")
 
-    def test_user_id_attr(self):
-        """Test Place has attr user_id, and it's an empty string"""
-        place = Place()
-        self.assertTrue(hasattr(place, "user_id"))
-        if models.storage_t == 'db':
-            self.assertEqual(place.user_id, None)
-        else:
-            self.assertEqual(place.user_id, "")
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_place(place_id):
+    """Delete place by id."""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    place.delete()
+    storage.save()
+    return jsonify({}), 200
 
-    def test_name_attr(self):
-        """Test Place has attr name, and it's an empty string"""
-        place = Place()
-        self.assertTrue(hasattr(place, "name"))
-        if models.storage_t == 'db':
-            self.assertEqual(place.name, None)
-        else:
-            self.assertEqual(place.name, "")
 
-    def test_description_attr(self):
-        """Test Place has attr description, and it's an empty string"""
-        place = Place()
-        self.assertTrue(hasattr(place, "description"))
-        if models.storage_t == 'db':
-            self.assertEqual(place.description, None)
-        else:
-            self.assertEqual(place.description, "")
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
+def create_place(city_id):
+    """Create place."""
+    dictionary = request.get_json()
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    if dictionary is None:
+        abort(400, 'Not a JSON')
+    if 'user_id' not in dictionary:
+        abort(400, 'Missing user_id')
+    if storage.get(User, dictionary['user_id']) is None:
+        abort(404)
+    if 'name' not in dictionary:
+        abort(400, 'Missing name')
+    dictionary['city_id'] = city_id
+    place = Place(**dictionary)
+    place.save()
+    return make_response(jsonify(place.to_dict()), 201)
 
-    def test_number_rooms_attr(self):
-        """Test Place has attr number_rooms, and it's an int == 0"""
-        place = Place()
-        self.assertTrue(hasattr(place, "number_rooms"))
-        if models.storage_t == 'db':
-            self.assertEqual(place.number_rooms, None)
-        else:
-            self.assertEqual(type(place.number_rooms), int)
-            self.assertEqual(place.number_rooms, 0)
 
-    def test_number_bathrooms_attr(self):
-        """Test Place has attr number_bathrooms, and it's an int == 0"""
-        place = Place()
-        self.assertTrue(hasattr(place, "number_bathrooms"))
-        if models.storage_t == 'db':
-            self.assertEqual(place.number_bathrooms, None)
-        else:
-            self.assertEqual(type(place.number_bathrooms), int)
-            self.assertEqual(place.number_bathrooms, 0)
-
-    def test_max_guest_attr(self):
-        """Test Place has attr max_guest, and it's an int == 0"""
-        place = Place()
-        self.assertTrue(hasattr(place, "max_guest"))
-        if models.storage_t == 'db':
-            self.assertEqual(place.max_guest, None)
-        else:
-            self.assertEqual(type(place.max_guest), int)
-            self.assertEqual(place.max_guest, 0)
-
-    def test_price_by_night_attr(self):
-        """Test Place has attr price_by_night, and it's an int == 0"""
-        place = Place()
-        self.assertTrue(hasattr(place, "price_by_night"))
-        if models.storage_t == 'db':
-            self.assertEqual(place.price_by_night, None)
-        else:
-            self.assertEqual(type(place.price_by_night), int)
-            self.assertEqual(place.price_by_night, 0)
-
-    def test_latitude_attr(self):
-        """Test Place has attr latitude, and it's a float == 0.0"""
-        place = Place()
-        self.assertTrue(hasattr(place, "latitude"))
-        if models.storage_t == 'db':
-            self.assertEqual(place.latitude, None)
-        else:
-            self.assertEqual(type(place.latitude), float)
-            self.assertEqual(place.latitude, 0.0)
-
-    def test_longitude_attr(self):
-        """Test Place has attr longitude, and it's a float == 0.0"""
-        place = Place()
-        self.assertTrue(hasattr(place, "longitude"))
-        if models.storage_t == 'db':
-            self.assertEqual(place.longitude, None)
-        else:
-            self.assertEqual(type(place.longitude), float)
-            self.assertEqual(place.longitude, 0.0)
-
-    @unittest.skipIf(models.storage_t == 'db', "not testing File Storage")
-    def test_amenity_ids_attr(self):
-        """Test Place has attr amenity_ids, and it's an empty list"""
-        place = Place()
-        self.assertTrue(hasattr(place, "amenity_ids"))
-        self.assertEqual(type(place.amenity_ids), list)
-        self.assertEqual(len(place.amenity_ids), 0)
-
-    def test_to_dict_creates_dict(self):
-        """test to_dict method creates a dictionary with proper attrs"""
-        p = Place()
-        new_d = p.to_dict()
-        self.assertEqual(type(new_d), dict)
-        self.assertFalse("_sa_instance_state" in new_d)
-        for attr in p.__dict__:
-            if attr is not "_sa_instance_state":
-                self.assertTrue(attr in new_d)
-        self.assertTrue("__class__" in new_d)
-
-    def test_to_dict_values(self):
-        """test that values in dict returned from to_dict are correct"""
-        t_format = "%Y-%m-%dT%H:%M:%S.%f"
-        p = Place()
-        new_d = p.to_dict()
-        self.assertEqual(new_d["__class__"], "Place")
-        self.assertEqual(type(new_d["created_at"]), str)
-        self.assertEqual(type(new_d["updated_at"]), str)
-        self.assertEqual(new_d["created_at"], p.created_at.strftime(t_format))
-        self.assertEqual(new_d["updated_at"], p.updated_at.strftime(t_format))
-
-    def test_str(self):
-        """test that the str method has the correct output"""
-        place = Place()
-        string = "[Place] ({}) {}".format(place.id, place.__dict__)
-        self.assertEqual(string, str(place))
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
+def modify_place(place_id):
+    """Modify the place obj."""
+    place = storage.get(Place, place_id)
+    dictionary = request.get_json()
+    if place is None:
+        abort(404)
+    if dictionary is None:
+        abort(400, 'Not a JSON')
+    ignored_key = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
+    for key, value in dictionary.items():
+        if key not in ignored_key:
+            setattr(place, key, value)
+    place.save()
+    return make_response(jsonify(place.to_dict()), 200)
