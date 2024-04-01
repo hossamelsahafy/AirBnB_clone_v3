6b7c46cb7905ca711e3d919ba98aefefe3e70f47
Mohamed kamal <kmido9729@gmail.com>
2024-04-02 00:14:31 +0200

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,13 @@
-#!/usr/bin/python3
-""" config app """
-from flask import Flask, jsonify
+from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-
-host = getenv("HBNB_API_HOST", "0.0.0.0")
-port = getenv("HBNB_API_PORT", "5000")
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+# Allow CORS for all domains on all routes
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage"""
-    storage.close()
-
-
-@app.errorhandler(404)
-def page_not_found(exception):
-    """ found 404 error """
-    return jsonify({"error": "Not found"}), 404
-
-
-if __name__ == '__main__':
-    app.run(host=host, port=port, threaded=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, threaded=True)
