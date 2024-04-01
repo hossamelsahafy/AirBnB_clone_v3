ecd70f17147752d727987b492e983e7739dc35a7
Mohamed kamal <kmido9729@gmail.com>
2024-04-02 00:43:13 +0200

    hamo bydahy

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,32 @@
-from flask import Flask, Blueprint, jsonify
-from models import storage
-from api.v1.views import app_views
-from flask_cors import CORS
+#!/usr/bin/python3
+"""Creating flask app."""
 
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import storage
+from os import getenv
+from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-# Allow CORS for all domains on all routes
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    """Close the storage."""
+    storage.close()
+
+
+@app.errorhandler(404)
+def error_handler(error):
+    """Handle for 404 errors."""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+
+    app.run(host=host, port=port, threaded=True)
